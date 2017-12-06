a41c57dd2f41a24f64dfe6e00eb7ba07656d2266
phuongdang9201 <phuongdang9201@gmail.com>
2017-12-06 23:49:03 +0700

    Add third activity

---
+++ app/src/main/java/com/example/dnp/demogit/ThirdActivity.java
@@ -0,0 +1,16 @@
+package com.example.dnp.demogit;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by DNP on 12/6/2017.
+ */
+
+public class ThirdActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
